add7e08f9d230d63c63b141f248956e1f170c0fc
itsdevruba <rubaaldawssari@gmail.com>
2026-09-10 17:42:38 +0300

    the doc

--- func.py
+++ func.py
@@ -1,5 +1,6 @@
 
 def print_numbers (number):
+    """ Prints a descending number pattern from the given number to 1. """
     for i in range(number,0,-1):
         for j in range(i,0,-1):
             print (j,end=" ")
@@ -11,6 +12,8 @@ def print_numbers (number):
 number = int(input("enter the number:"))
 
 print_numbers(number)
+
+print(print_numbers.__doc__)
 
 
 
